8e88ae9fdf8569a0062728392c7bc23d36d8e203
Kayne Jeacocks <kayne.jeacocks@gmail.com>
2021-04-06 15:42:20 +0200

    Added orgname to global variables in BaseController

--- inc/Base/BaseController.php
+++ inc/Base/BaseController.php
@@ -10,6 +10,7 @@
     public $plugin_path;
     public $plugin_url;
     public $plugin;
+    public $orgname;
     public $api_token;
     public $process_token;
 
@@ -17,6 +18,7 @@
         $this->plugin_path = plugin_dir_path( dirname(__FILE__,  2) );
         $this->plugin_url = plugin_dir_url( dirname(__FILE__,  2) );
         $this->plugin = plugin_basename( dirname(__FILE__,  3) ) . '/peak15-link.php';
+        $this->orgname = get_option( 'orgname' );
         $this->api_token = get_option( 'api_token' );
         $this->process_token = get_option( 'process_token' );
     }
