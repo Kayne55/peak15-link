3606a58bfcdc3fc22afdd25aade841e3e8f7660f
Kayne55 <92627903+KodeSoftwareDev@users.noreply.github.com>
2022-03-10 12:36:38 +0200

    Publishing to GitHub

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit4cfb641c4d1b477925ca44af8bb531e9
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit4cfb641c4d1b477925ca44af8bb531e9', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit4cfb641c4d1b477925ca44af8bb531e9', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,24 +1,23 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'staff1/peak15-link',
+        'dev' => true,
     ),
-    'reference' => NULL,
-    'name' => 'staff1/peak15-link',
-  ),
-  'versions' => 
-  array (
-    'staff1/peak15-link' => 
-    array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => NULL,
+    'versions' => array(
+        'staff1/peak15-link' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
     ),
-  ),
 );
